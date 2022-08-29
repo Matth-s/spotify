0a4befefdc07614d268807530d81b963adb68fdc
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-29 12:35:19 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,6 +1,13 @@
-const navbar = document.querySelector('#nav-header');
+const navbar = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
+const toggle = document.querySelector('.toggle');
 
 document.addEventListener('DOMContentLoaded', function(){
     navbar.classList.add('animation-header');
     console.log("page chargée");
+ });
+
+
+ toggle.addEventListener('click', function() {
+    body.classList.toggle('deroulant');
  });
